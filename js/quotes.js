cc71e8e6379484e8c416063d409bc18c1b8e5785
Skillz <admin@ezlgg.com>
2017-05-14 22:41:27 -0400

    completed all tasks but still remaining responsiveness

--- js/quotes.js
+++ js/quotes.js
@@ -1,62 +1,62 @@
 var quotes = [
     {
-        quote: "Life is about making an impact, not making an income.",
-        source: "Kevin Kruse",
-        citation: "Forbes Article",
-        year: "2017",
-        tags: "Inspirational",
-        rating: "3"
+        quote : "Life is about making an impact, not making an income.",
+        source : "Kevin Kruse",
+        citation : "Forbes Article",
+        year : "2017",
+        tags : "Inspirational",
+        rating : "3"
     },
     {
-        quote: "Strive not to be a success, but rather to be of value.",
-        source: "Albert Einstein",
+        quote : "Strive not to be a success, but rather to be of value.",
+        source : "Albert Einstein",
+        citation : "",
+        year : "",
+        tags : "Inspirational",
+        rating : "4"
+    },
+    {
+        quote : "If you let my daughter go now, that'll be the end of it. I will not look for you, I will not pursue you. But if you don't, I will look for you, I will find you, and I will kill you.",
+        source : "Liam Neeson",
+        citation : "Taken",
+        year : "2008",
+        tags : "Movies",
+        rating : "4"
+    },
+    {
+        quote : "I'll be back.",
+        source : "Arnold Schwarzenegger",
+        citation : "The Terminator",
+        year : "1984",
+        tags : "Movies",
+        rating : "3"
+    },
+    {
+        quote : "To love is nothing. To be loved is something. But to love and be loved, that’s everything.",
+        source : "T. Tolis",
+        citation : "",
+        year : "",
+        tags : "Love",
+        rating : "4"
+    },
+    {
+        quote : "The best and most beautiful things in this world cannot be seen or even heard, but must be felt with the heart.",
+        source : "Helen Keller ",
+        citation : "",
+        year : "",
+        tags : "Love",
+        rating : "4"
+    },
+    {
+        quote : "Education is the most powerful weapon which you can use to change the world.",
+        source : "Nelson Mendela",
         citation: "",
-        year: "",
-        tags: "Inspirational",
-        rating: "4"
+        year : "",
+        tags : "Change",
+        rating : "5"
     },
     {
-        quote: "If you let my daughter go now, that'll be the end of it. I will not look for you, I will not pursue you. But if you don't, I will look for you, I will find you, and I will kill you.",
-        source: "Liam Neeson",
-        citation: "Taken",
-        year: "2008",
-        tags: "Movies",
-        rating: "4"
-    },
-    {
-        quote: "I'll be back.",
-        source: "Arnold Schwarzenegger",
-        citation: "The Terminator",
-        year: "1984",
-        tags: "Movies",
-        rating: "3"
-    },
-    {
-        quote: "To love is nothing. To be loved is something. But to love and be loved, that’s everything.",
-        source: "T. Tolis",
-        citation: "",
-        year: "",
-        tags: "Love",
-        rating: "4"
-    },
-    {
-        quote: "The best and most beautiful things in this world cannot be seen or even heard, but must be felt with the heart.",
-        source: "Helen Keller ",
-        citation: "",
-        year: "",
-        tags: "Love",
-        rating: "4"
-    },
-    {
-        quote: "Education is the most powerful weapon which you can use to change the world."
-        source: "Nelson Mendela",
-        citation: "",
-        year: "",
-        tags: "Change",
-        rating: "5"
-    },
-    {
-        quote: "Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world."
+        quote: "Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.",
         source: "Harriet Tubman",
         citation: "",
         year: "",
@@ -64,19 +64,19 @@ var quotes = [
         rating: "5"
     },
     {
-        quote: "The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time.",
-        source: "Mark Twain",
-        citation: "",
-        year: "",
-        tags: "Death",
-        rating: "5"
+        quote : "The fear of death follows from the fear of life. A man who lives fully is prepared to die at any time.",
+        source : "Mark Twain",
+        citation : "",
+        year : "",
+        tags : "Death",
+        rating : "5"
     },
     {
-        quote: "If we lose love and self respect for each other, this is how we finally die.",
-        source: "Maya Angelou",
-        citation: "",
-        year: "",
-        tags: "Death",
-        rating: "5"
+        quote : "If we lose love and self respect for each other, this is how we finally die.",
+        source : "Maya Angelou",
+        citation : "",
+        year : "",
+        tags : "Death",
+        rating : "5"
     },
 ];
--- js/script.js
+++ js/script.js
@@ -2,3 +2,49 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//create all global variables
+var quote, source, citation, year, tags, rating, fullQuote;
+var numOfQuotes = quotes.length;
+var usedQuotes = [];
+/*infinite loop to find a random number which if is not already used goes through and assigns the object to variables and pushes the 
+random number to the usedquotes array and loggs in console and breaks the loop. If the random number is already in the usedquotes itll
+run again. If the usedquotes has every quote then itll reset. */
+function getRandomQuote () {
+    while (true) {
+        var randomNum = Math.floor(Math.random() * numOfQuotes);
+        if (usedQuotes.indexOf(randomNum) === -1) {
+            quote = quotes[randomNum].quote;
+            source = quotes[randomNum].source;
+            citation = quotes[randomNum].citation;
+            year = quotes[randomNum].year;
+            tags = quotes[randomNum].tags;
+            rating = quotes[randomNum].rating;
+            usedQuotes.push(randomNum);
+            console.log(quote);
+            break;
+        } else if (usedQuotes.length === quotes.length) {
+            usedQuotes = [];
+        } } }
+//print the quote by creating different html codes depending on if a citation and year is present or not.
+function printQuote () {
+    getRandomQuote();
+    if (citation !== "" && year !== "") {
+        fullQuote = '<p class="quote">' + quote + '</p><p class="source">' + source + '<span class="citation">' + 
+        citation + '</span><span class="year">' + year + '</span></p><h3 class="source">' + tags + '</h3><h3 class="source">Rated: ' +
+        rating + 'stars</h3>'
+    } else if (citation !== "") {
+        fullQuote = '<p class="quote">' + quote + '</p><p class="source">' + source + '<span class="year">' + year +
+        '</span></p><h3 class="source">' + tags + '</h3><h3 class="source">Rated: ' + rating + 'stars</h3>'
+    } else { 
+        fullQuote = '<p class="quote">' + quote + '</p><p class="source">' + source + '<span class="citation">' + 
+        citation + '</span></p><h3 class="source">' + tags + '</h3><h3 class="source">Rated: ' +
+        rating + 'stars</h3>'
+    }
+    document.getElementById("quote-box").innerHTML = fullQuote;
+    //random color generator
+    document.body.style.backgroundColor = '#'+Math.random().toString(16).slice(-6);
+}
+//random quote after every 5 seconds
+setInterval(printQuote, 5000);
+
+
