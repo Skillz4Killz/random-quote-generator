506da464f114ba1e3ed2e779f146c7e870dafed8
Skillz <admin@ezlgg.com>
2017-05-17 15:32:05 -0400

    fixed the Button background color not changing

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 var quote, source, citation, date, tags, rating, fullQuote;
 var numOfQuotes = quotes.length;
 var usedQuotes = [];
+var loadQuote = document.getElementById("loadQuote");
+
 /*infinite loop to find a random number which if is not already used goes through and assigns the object to variables and pushes the 
 random number to the usedquotes array and loggs in console and breaks the loop. If the random number is already in the usedquotes itll
-run again. If the usedquotes has every quote then itll reset. */
+run again. If the usedquotes has every quote > reset. */
 function getRandomQuote () {
     while (true) {
         var randomNum = Math.floor(Math.random() * numOfQuotes);
@@ -25,6 +27,7 @@ function getRandomQuote () {
         } else if (usedQuotes.length === quotes.length) {
             usedQuotes = [];
         } } }
+
 //print the quote by creating different html codes depending on if a citation and date is present or not.
 function printQuote () {
     getRandomQuote();
@@ -42,9 +45,10 @@ function printQuote () {
     }
     document.getElementById("quote-box").innerHTML = fullQuote;
     //random color generator
-    document.body.style.backgroundColor = '#'+Math.random().toString(16).slice(-6);
+    randomColor = '#'+Math.random().toString(16).slice(-6);
+    document.body.style.backgroundColor = randomColor;
+    document.getElementById("loadQuote").style.backgroundColor = randomColor;
 }
-//random quote after every 5 seconds
-setInterval(printQuote, 5000);
 
-
+//random quote after every 30 seconds. This takes into account Unique quotes also.
+setInterval(printQuote, 30000);
